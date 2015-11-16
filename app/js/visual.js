8f0b53b9af9286f5389aaa80e65ad6af144d60b4
tblumer <blumer.tanner@gmail.com>
2015-11-16 00:07:11 -0500

    adding immunizations and prior diagnosis

--- app/js/visual.js
+++ app/js/visual.js
@@ -1,13 +1,12 @@
-$(function () {
-    $('#prior').change(function(){
-        var value = this.checked;
-        console.log(value);
-        if (value === true) {
-            $('#prior-medical').show();
-            console.log("true!");
-        } else {
-            $('#prior-medical').hide(100);
-            console.log("false!");
-        }
-    });
-});
+function show_sub(event) {
+    var div_id = event.target.id;
+    var value = $('#'+div_id).prop('checked');
+    var follow_div_id = div_id + "-follow";
+
+    if (value) {
+        $('#'+follow_div_id).show();
+    } else {
+        $('#'+follow_div_id).hide();
+    }
+    return true;
+}
